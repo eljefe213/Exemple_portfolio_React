651e21df1ec0521f13e938d8ca44c701d9c95c9f
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2023-04-24 12:38:39 +0200

    Exemple_portfolio_React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import About from './components/about/About'
-import Contact from './components/contact/Contact'
-import Experience from './components/experience/Experience'
-import Footer from './components/footer/Footer'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import Portfolio from './components/portfolio/Portfolio'
-import Services from './components/services/Services'
-import Testimonials from './components/testimonials/Testimonials'
+import React from "react";
+import ReactDOM from "react-dom";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
 
 const App = () => {
   return (
@@ -22,7 +23,7 @@ const App = () => {
       <Contact />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import  ReactDOM  from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
-import './index.css';
+import "./index.css";
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+ReactDOM.render(<App />, document.querySelector("#root"));
